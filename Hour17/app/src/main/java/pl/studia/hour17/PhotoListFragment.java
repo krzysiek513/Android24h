add486dbee3e954b7548ff3027ab856ee7d943fa
Krzysiek <krzysiek3155@gmail.com>
2021-05-25 11:00:10 +0200

    work with remote API

---
+++ Hour17/app/src/main/java/pl/studia/hour17/PhotoListFragment.java
@@ -0,0 +1,22 @@
+package pl.studia.hour17;
+
+import java.util.ArrayList;
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class PhotoListFragment extends ListFragment    {
+	String[] mTitles;	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		MainActivity currentActivity = (MainActivity) this.getActivity();
+		ArrayList <FlickrPhoto> photos = currentActivity.getPhotos();
+		mTitles = new String[photos.size()];
+		for (int i=0; i < photos.size(); i++){
+			mTitles[i] = photos.get(i).title;
+		}
+        setListAdapter(new ArrayAdapter<String>(this.getActivity(),
+                android.R.layout.simple_list_item_1, mTitles));
+	}
+}
